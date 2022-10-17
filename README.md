cf84375d25d4b4d2b7b09706c83c163db79c379d
Rosie Hamilton <enchantrix@gmail.com>
2022-10-17 14:58:54 +0100

    add benchmarks

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ https://www.youtube.com/watch?v=2557w0qsDV0
 
 Note that Google found an expanded example with 2 more algos at:
 https://go.dev/play/p/9h53-8jZUW
+
+## Benchmarks
+
+To run benchmarks
+`go test -run none -bench . -benchtime 3s -benchmem`
+
+Starting code outputs:
+```
+BenchmarkAlgOne-16       2452267              1472 ns/op              53 B/op          2 allocs/op
+BenchmarkAlgTwo-16       8997981               395.8 ns/op             0 B/op          0 allocs/op
+```
---
+++ main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func BenchmarkAlgOne(b *testing.B) {
+	var output bytes.Buffer
+	in := assembleInputStream()
+	find := []byte("elvis")
+	repl := []byte("Elvis")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		output.Reset()
+		algOne(in, find, repl, &output)
+	}
+}
+
+func BenchmarkAlgTwo(b *testing.B) {
+	var output bytes.Buffer
+	in := assembleInputStream()
+	find := []byte("elvis")
+	repl := []byte("Elvis")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		output.Reset()
+		algTwo(in, find, repl, &output)
+	}
+}
